4a527b8657704d805ca682e835273d7f88b3e4ae
Neto peixoto <joseneto55peixoto@gmail.com>
2023-07-07 09:42:45 -0300

    files novos

---
+++ recipes/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = "recipes"
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('recipes/category<int:category_id>/',
+         views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
+
+
+]
