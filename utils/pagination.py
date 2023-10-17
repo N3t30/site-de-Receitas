ac95d89d35a6a90cf161e20f955078a9b193cb82
Neto peixoto <joseneto55peixoto@gmail.com>
2023-10-16 22:44:40 -0300

    testes author

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,6 +1,20 @@
+import math
+
+
 def make_pagination_range(
         page_range,
-        qtd_paginas,
+        qtd_pages,
         current_page,
 ):
-    return [1, 2, 3, 4]
+    middle_range = math.ceil(qtd_pages / 2)  # meio das pages ranges
+    start_range = current_page - middle_range  # Começa dois numeros antes da middle_range # noqa: E501
+    stop_range = current_page + middle_range  # para dois numeros do middle page # noqa: E501
+
+    # "abs" tira o sinal de negativo,
+    start_range_offset = abs(start_range) if start_range < 0 else 0
+
+    if start_range < 0:
+        start_range = 0
+        stop_range += start_range_offset
+
+    return page_range[start_range: stop_range]
--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -8,8 +8,35 @@ class PaginationTest(TestCase):
         pagination = make_pagination_range(
             # total de paginas que tem disponivel
             page_range=list(range(1, 21)),
-            qtd_paginas=4,  # quantidades de paginas que serão mostradas ao usuario # noqa: E501
+            qtd_pages=4,  # quantidades de paginas que serão mostradas ao usuario # noqa: E501
             current_page=1,  # Em qual pagina o usuario estar
         )
         # Lista nova dentro do pagination, será retornado o range de 4pags
         self.assertEqual([1, 2, 3, 4], pagination)
+
+    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa: E501
+        pagination = make_pagination_range(
+            # total de paginas que tem disponivel
+            page_range=list(range(1, 21)),
+            qtd_pages=4,  # quantidades de paginas que serão mostradas ao usuario # noqa: E501
+            current_page=1,  # Em qual pagina o usuario estar
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)  # Lista nova dentro do pagination, será retornado o range de 4pags # noqa: E501
+
+        pagination = make_pagination_range(
+            # total de paginas que tem disponivel
+            page_range=list(range(1, 21)),
+            qtd_pages=4,  # quantidades de paginas que serão mostradas ao usuario # noqa: E501
+            current_page=2,  # Em qual pagina o usuario estar
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)  # Lista nova dentro do pagination, será retornado o range de 4pags # noqa: E501
+
+        # Aqui o intervalo deve mudar
+
+        pagination = make_pagination_range(
+            # total de paginas que tem disponivel
+            page_range=list(range(1, 21)),
+            qtd_pages=4,  # quantidades de paginas que serão mostradas ao usuario # noqa: E501
+            current_page=3,  # Em qual pagina o usuario estar
+        )
+        self.assertEqual([2, 3, 4, 5], pagination)  # Lista nova dentro do pagination, será retornado o range de 4pags # noqa: E501
