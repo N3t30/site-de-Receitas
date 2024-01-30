0d61f1e95f60ce660f17f3d8c712568ae6943e68
Neto peixoto <joseneto55peixoto@gmail.com>
2024-01-29 21:18:42 -0300

    toques finais no layout do formulário de criação de receitas

--- authors/forms/recipe_forms.py
+++ authors/forms/recipe_forms.py
@@ -1,11 +1,37 @@
 from django import forms
 
 from recipes.models import Recipe
+from utils.django_forms import add_attr
 
 
 class AuthorRecipeForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        add_attr(self.fields.get('cover'), 'class', 'span-2')
+
     class Meta:
         model = Recipe
         fields = 'title', 'description', 'preparation_time', \
             'preparation_time_unit', 'servings', 'servings_unit', \
             'preparation_steps', 'cover'
+        widgets = {
+            'cover': forms.FileInput(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+            'servings_unit': forms.Select(
+                choices=(
+                    ('Porções', 'Porções'),
+                    ('Pedaços', 'Pedaços'),
+                    ('pessoas', 'Pessoas'),
+                ),
+            ),
+            'sreparation_time_unit': forms.Select(
+                choices=(
+                    ('Minutos', 'Minutos'),
+                    ('Horas', 'Horas'),
+                ),
+            ),
+        }
--- authors/urls.py
+++ authors/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path(
         'dashboard/recipe/<int:id>/edit/',
         views.dashboard_recipe_edit,
-        name='dashboard_recipe_edit'
+        name='dashboard_recipe_edit',
     ),
 ]
