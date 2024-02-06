df29662c3acfce871e10a8fd4199f2bf942c817a
Neto peixoto <joseneto55peixoto@gmail.com>
2024-02-05 22:07:42 -0300

    CRUD com delete para apagar receitas de forma segura + javascript adcional

--- authors/urls.py
+++ authors/urls.py
@@ -23,4 +23,9 @@ urlpatterns = [
         views.dashboard_recipe_edit,
         name='dashboard_recipe_edit',
     ),
+    path(
+        'dashboard/recipe/<int:id>/delete/',
+        views.dashboard_recipe_delete,
+        name='dashboard_recipe_delete'
+    ),
 ]
